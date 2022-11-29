6929d8cf17b2b1f4b07b4bb8f1dc4bc8c418479d
Brajananda Mahata <brajanandamahato28603@gmail.com>
2022-11-30 00:28:42 +0530

    Day 2 Commit

---
+++ day2/main.py
@@ -0,0 +1,7 @@
+print("discussion about projects and example projects shown in the video ")
+print("\nProject 1 : JARVIS ASSISTANT")
+print("\nProject 2 : Love Calculator - randomly calculates love between two people given names")
+print("\nProject 3 : Face Detection Software")
+print("\nProject 4 : Flappy Bird Game")
+print("\nProject 5 : Snake Game")
+print("\n Download Python codes of above games , links given in video description")
