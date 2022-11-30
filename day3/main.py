5f4b008191bf42b992bb7ce312ac5304ba4a9e9b
Brajananda Mahata <brajanandamahato28603@gmail.com>
2022-11-30 21:20:59 +0530

    day3 commit

---
+++ day3/main.py
@@ -0,0 +1,6 @@
+import hashlib # here we imported a built in module
+import pandas # here we imported an external module , installed using pip installer
+print("this day we learn about modules: example pandas using the pip package manager")
+print("also built-in and external modules")
+
+
