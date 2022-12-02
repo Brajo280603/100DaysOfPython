c1488829ee1cb68a78865437ace1e2ae572488cd
Brajananda Mahata <brajanandamahato28603@gmail.com>
2022-12-03 00:40:25 +0530

    day 5 commit

---
+++ day5/main.py
@@ -0,0 +1,18 @@
+print("Hey I am a good boy \n and the viewer of this code is also a good boy/girl") #\n is a newline character it introduces a newLine it will enter a newLine in the output
+
+#this is a single line comment , use it by starting it with "#"
+
+''' 
+this is a multiline comment use it by inserting a comment between triple ' or triple "
+'''
+
+#escape sequence characters
+
+# if a character cannot be used direcly in print function use a escape sequence character
+
+print("yo look at this \\") #if a single \ was used this would give an error so we use \ as an escape sequence and pass \\ to print the \ in output.
+
+print ("to escape a tab use \t look! look again\t!")
+
+print("Hey " , "look " , "what " , 1 , "learned" , "about separators", sep="@") # this will  use @ as separator and separate the values given to print , if no sep is given _blank_space_ is used
+
