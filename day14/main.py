d453b3b46a4b39b3f5a5c4e0bfc7fcee14bc7b35
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-26 17:42:34 +0530

    day14 commit

---
+++ day14/main.py
@@ -0,0 +1,14 @@
+budget = 500
+price = int(input("Enter the price:"))
+
+if (price>budget):
+  print("you cannot afford")
+elif (price == budget):
+  print("you can afford , but not recommended")
+  #in python indentation matters , this comment is in elseif(elif) block
+else:
+  print("you can afford, buy it now")
+  #this one is in else block
+#this is outside 
+
+#nested if else statements can be written using indentation
