2145e728299093d23af0e16b386a948202966438
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-26 18:04:08 +0530

    day 15

---
+++ day15/main.py
@@ -0,0 +1,13 @@
+#excersize goodmorning sir
+import time
+
+timestamp = int(time.strftime("%H"))
+
+name = input("Enter your name:")
+
+if(timestamp<12):
+  print("good morning,"+name)
+elif(timestamp>12 and timestamp<18):
+  print("good afternoon,"+name)
+else:
+  print("good evening,"+name)
