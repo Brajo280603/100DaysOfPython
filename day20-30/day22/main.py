8322922dace1ff54bf38db7c36ec3c9b356e7440
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 15:46:27 +0530

    day22 commit

---
+++ day20-30/day22/main.py
@@ -0,0 +1,13 @@
+# l = [2,3,4,5,6] #this is a python list (basically array)
+
+# print(l)
+# print(type(l))
+
+# #every operations apply to strings as well
+# print(l[1:3])#this will print from 1 to 2 index||| l[start:end-1:step]
+
+#list comprehension || generating lists on the fly
+lst = [i for i in range(4)] 
+lsted = [i*2 for i in range(5)]
+
+print(lsted) 
