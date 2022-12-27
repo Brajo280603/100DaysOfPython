3b840531edd7c4274895ad1ad3b1b919c78fa143
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 16:09:49 +0530

    day27 precommit

---
+++ day20-30/day27excz3/main.py
@@ -0,0 +1,3 @@
+#Creat a program capable of displaying questions to the user like KBC
+# Use List data type to store the questions and their correct answers.
+# Display the final amount the person is taking home after playing
