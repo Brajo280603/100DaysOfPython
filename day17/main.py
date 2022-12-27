b99119ff3872d1c2fefcd23e58cebfe50548eb60
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 14:32:37 +0530

    day17 commit

---
+++ day17/main.py
@@ -0,0 +1,16 @@
+# name = "ABHISEK"
+# for i in name:
+#   print(i)
+#   if(i=="B"):
+#     print("lol")
+
+
+#NESTED for loop
+# colors = ["red", "green", "blue", "yellow"]
+# for color in colors:
+#   print(color)
+#   for i in color:
+#     print(i)
+
+for k in range(): #range(start, end, step) start is by default 0 
+  print(k)
