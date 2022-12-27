a3bbaaee93830f619f304b2739df4bd23bf0bd23
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 15:57:32 +0530

    day24

---
+++ day20-30/day24/main.py
@@ -0,0 +1,7 @@
+tup = (1,5,6,7,8,9,10) #this is a tuple , values are nonchangeable
+
+onetup=(1,)#a comma is necessary to make a single element tuple, 
+
+
+tup2 = tup[1:4] #all methods that are applicable on list noted that they dont change the original input will be applicable on tuples
+print(type(tup),tup,tup2)
