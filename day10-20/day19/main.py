326763410f97c1067e24b3685a25fbfb5b00e655
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 14:53:44 +0530

    day19 commit

---
+++ day10-20/day19/main.py
@@ -0,0 +1,18 @@
+# for i in range(12):
+#   if(i == 10):
+#     break
+#   print("5 X", i+1, "=",5*(i+1))
+#   #break statments breaks the loop
+
+# for i in range(12):
+#   if(i == 10):
+#     continue
+#   print("5 X", i, "=",5*(i))
+#   #continue statments skips the iteration
+
+i = 0
+while True: #this makes an infinite loop
+  print(i)
+  i = i+1
+  if(i== 100):
+    break
