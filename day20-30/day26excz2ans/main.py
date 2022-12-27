992d865affec9155eac5d6abe18ec85500871f3e
Brajananda Mahata <brajanandamahato280603@gmail.com>
2022-12-27 16:07:34 +0530

    day26 commit

---
+++ day20-30/day26excz2ans/main.py
@@ -0,0 +1,16 @@
+import time 
+
+t = time.strftime('%H:%M:%S')
+
+hour = int(time.strftime("%H"))
+print(t)
+print(hour)
+
+if(hour>0 and hour<12):
+  print("Good Morning Sir!")
+elif(hour==12):
+  print("Good Noon Sir!")
+elif(hour>12 and hour<18):
+  print("Good AfterNoon Sir!")
+elif(hour>=18):
+  print("Good Evening Sir!")
